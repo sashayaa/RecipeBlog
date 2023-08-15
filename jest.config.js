b1f1aec57d0895645d083def784f13a1e2609720
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2023-08-15 17:16:58 +0100

    Configure jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {tsconfig: 'tsconfig.jest.json'}]
+  },
+  setupFilesAfterEnv: ['./src/jest.setup.ts'],
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main>
       <div>
-       <h1>Home</h1>
-       <p>Hello</p>
+        <h1>Home</h1>
+        <p>Hello</p>
       </div>
     </main>
   );
